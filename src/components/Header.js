eeb33ed81eb5dd27ef81932c8d2011612eb23cfb
doshiparth642 <doshiparth642@gmail.com>
2022-10-17 18:00:45 +0530

    chenges in updatefile

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <div className="ui fixed menu">
             <div className="ui container center">
+                <h1>Hello, How Are You?</h1>
                 <h2>Data Manager</h2>
             </div>
         </div>
