b5cd73eae1a49d4ca64716e2a0d26b3920700803
doshiparth642 <doshiparth642@gmail.com>
2022-10-17 18:11:23 +0530

    dfdfdfdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,8 @@ const Header = () => {
             <div className="ui container center">
                 <h1>Hello, How Are You?</h1>
                 <h2>Data Manager</h2>
+                <h3>How was your age?</h3>
+                <h4>Dummy Data is added</h4>
             </div>
         </div>
     )
