aec8871d7fb3193462c32b0b56effbbc610a8d12
doshiparth642 <doshiparth642@gmail.com>
2022-10-17 18:01:49 +0530

    dfdfdf

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ const Header = () => {
     return (
         <div className="ui fixed menu">
             <div className="ui container center">
+                <h1>Why Are You?</h1>
                 <h2>Contact Value Manager</h2>
             </div>
         </div>
